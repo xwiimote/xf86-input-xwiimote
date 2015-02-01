54ac664a2f334b5d06367dade89d34a7a4faa6d4
zakkudo <zakkudo2@gmail.com>
2015-01-31 22:31:29 -0800

    Added xorg props to the driver so userland can change settings

--- src/xwiimote.c
+++ src/xwiimote.c
@@ -45,35 +45,10 @@
 #include <xserver-properties.h>
 #include <xwiimote.h>
 
-#include "wiimote.h"
-#include "nunchuk.h"
-
-enum key_layout {
-	KEY_LAYOUT_DEFAULT,
-	KEY_LAYOUT_IR,
-	KEY_LAYOUT_NUM
-};
+#include "xwiimote.h"
+#include "properties.h"
 
 static char xwiimote_name[] = "xwiimote";
-
-struct xwiimote_dev {
-	InputInfoPtr info;
-	void *handler;
-	int dev_id;
-	char *root;
-	const char *device;
-	bool dup;
-	struct xwii_iface *iface;
-	unsigned int ifs;
-  enum key_state motion_layout;
-
-	XkbRMLVOSet rmlvo;
-
-  struct wiimote wiimote;
-  struct wiimote_config wiimote_config[KEY_LAYOUT_NUM];
-  struct nunchuk nunchuk;
-  struct nunchuk_config nunchuk_config[KEY_LAYOUT_NUM];
-};
 
 /* List of all devices we know about to avoid duplicates */
 static struct xwiimote_dev *xwiimote_devices[MAXDEVICES + 1];
@@ -298,6 +273,8 @@ static int xwiimote_init(struct xwiimote_dev *dev, DeviceIntPtr device)
       ret = Success;
       break;
 	}
+
+  xwiimote_initialize_properties(device, dev);
 
 	if (ret != Success)
 		return ret;
